0cd7e317d20f6031c4e074b3306c2a0e1f4823c9
parsher <futureslabor@gmail.com>
2019-10-18 16:29:06 +0900

    Initial commit

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,7 @@
+const GOOGLE_API_KEY = 'AIzaSyAOYx963AjQAAyuzaE8ue5QpbSyr2MbEBU';
+
+class LocationHelper {
+  static String generateLocationPreviewImage({double latitude, double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/greate_places.dart';
+import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
+      ),
+    );
+  }
+}
