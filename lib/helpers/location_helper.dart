9be542d614863f8a7b5fedc865cf86553bb55d93
parsher <futureslabor@gmail.com>
2019-10-18 16:34:37 +0900

    static map api url

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -2,6 +2,6 @@ const GOOGLE_API_KEY = 'AIzaSyAOYx963AjQAAyuzaE8ue5QpbSyr2MbEBU';
 
 class LocationHelper {
   static String generateLocationPreviewImage({double latitude, double longitude}) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%$latitude,$longitude&key=$GOOGLE_API_KEY';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red|label:A|$latitude,$longitude&key=$GOOGLE_API_KEY';
   }
 }
